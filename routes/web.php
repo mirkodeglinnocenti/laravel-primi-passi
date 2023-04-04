bada6590611347d67975586afe876b4bf84a9306
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-04-04 16:19:39 +0200

    FEAT about,contacts,services page added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
